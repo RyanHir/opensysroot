1f8f5780da5eeb0d7af8cf20d22ddbc079b2e50b
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-08-25 21:26:26 -0500

    Go to classic config

--- setup.py
+++ setup.py
@@ -1,2 +1,14 @@
-import setuptools
-setuptools.setup()
+
+from setuptools import setup
+
+setup(
+    name='opensysroot',
+    version='0.0.1',
+    packages=[
+        'opensysroot'
+        'opensysroot/enums'
+    ],
+    install_requires=[
+        'requests',
+    ],
+)
