eeb02c8de762d5ffe80706afecde306ed79752ba
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2022-04-08 11:17:39 -0500

    Use 2021 repos for RoboRIO

--- opensysroot/consts.py
+++ opensysroot/consts.py
@@ -3,7 +3,7 @@ from .enums.distro import Distro
 REPOS = {
     Distro.DEBIAN: "http://ftp.debian.org/debian/",
     Distro.RASPBIAN: "http://archive.raspbian.org/raspbian",
-    Distro.ROBORIO: "http://download.ni.com/ni-linux-rt/feeds/2019/arm/cortexa9-vfpv3/",
+    Distro.ROBORIO: "https://download.ni.com/ni-linux-rt/feeds/2021.5/arm/main/cortexa9-vfpv3/",
     Distro.UBUNTU: {"port": "http://ports.ubuntu.com/ubuntu-ports/",
                     "archive": "http://archive.ubuntu.com/ubuntu/"}
 }
